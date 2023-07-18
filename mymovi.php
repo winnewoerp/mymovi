f8e141099101c51b6e1fe7520e3c84173c37abd2
Stadtkreation <git@stadtkreation.de>
2023-07-18 08:11:32 +0200

    Version 0.2 - form basics added

--- mymovi.php
+++ mymovi.php
@@ -38,7 +38,58 @@ if ( is_admin() ) {
     require_once __DIR__ . '/admin/class-mymovi-admin.php';
 }
 
-function mymovi_test($content) {
+function mymovi_posttype_init() {
+	$mymovi_posttypes = array(
+		array(
+			'name' => 'mmv-survey-entry',
+			'slug' => 'survey-entry',
+			'singular' => __('Survey entry','mymovi'),
+			'plural' => __('Survey entries','mymovi')
+		),
+	);
+	foreach($mymovi_posttypes as $mymovi_posttype) {
+		$menu_name = (isset($mymovi_posttype['menu']) ? $mymovi_posttype['menu'] : $mymovi_posttype['plural']);
+		$labels = array(
+			'name'               => _x( $mymovi_posttype['plural'], 'post type general name', 'mymovi' ),
+			'singular_name'      => _x( $mymovi_posttype['singular'], 'post type singular name', 'mymovi' ),
+			'menu_name'          => _x( $menu_name, 'admin menu', 'mymovi' ),
+			'name_admin_bar'     => _x( $mymovi_posttype['singular'], 'add new on admin bar', 'mymovi' ),
+			'add_new'            => _x( 'Add New', 'mymoviproposal', 'mymovi' ),
+			'add_new_item'       => sprintf(__( 'Add New %s', 'mymovi' ),$mymovi_posttype['singular']),
+			'new_item'           => sprintf(__( 'New %s', 'mymovi' ),$mymovi_posttype['singular']),
+			'edit_item'          => sprintf(__( 'Edit %s', 'mymovi' ),$mymovi_posttype['singular']),
+			'view_item'          => sprintf(__( 'View %s', 'mymovi' ),$mymovi_posttype['singular']),
+			'all_items'          => sprintf(__( 'All %s', 'mymovi' ),$mymovi_posttype['plural']),
+			'search_items'       => sprintf(__( 'Search %s', 'mymovi' ),$mymovi_posttype['plural']),
+			'parent_item_colon'  => sprintf(__( 'Parent %s:', 'mymovi' ),$mymovi_posttype['plural']),
+			'not_found'          => sprintf(__( 'No %s found.', 'mymovi' ),$mymovi_posttype['plural']),
+			'not_found_in_trash' => sprintf(__( 'No %s found in Trash.', 'mymovi' ),$mymovi_posttype['plural'])
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'show_in_rest'		 => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => $mymovi_posttype['slug'] ),
+			'capability_type'    => 'post',
+			'has_archive'        => true,
+			'hierarchical'       => false,
+			'menu_position'      => 5,
+			'taxonomies' => array('category','post_tag'),
+			'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields', 'revisions'  )
+		);
+
+		register_post_type( $mymovi_posttype['name'], $args );
+		add_post_type_support( $mymovi_posttype['name'],  array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields', 'revisions' ) );
+	}
+}
+add_action( 'init', 'mymovi_posttype_init' );
+
+function mymovi_test_shortcode($content) {
 	$content .= wpautop('
 	<h2>MyMoVi <br>My Mobility Vision plugin test section</h2> 
 	<strong>TEST: Yes, MyMoVi plugin is working!</strong> This test output appears below all page and post contents. Start drawing (with routes calculated between points).
@@ -51,11 +102,173 @@ function mymovi_test($content) {
   </select>
   <input class="form-control" type="button" value="Remove last point (only for LineString and Polygon)" id="undo">
   	<p>Street distance: <span id="street-distance">0</span></p>
-	<div id="ifl-map"></div>  
+	<div class="mymovi-map" id="ifl-map"></div>
+	<script> addMap("ifl-map"); </script>
 	');
 	return $content;
 }
-add_filter('the_content', 'mymovi_test');
+add_shortcode('mymovi-test', 'mymovi_test_shortcode');
+
+function mymovi_form_shortcode($atts, $content) {
+	$output = '';
+	
+	$form_pages = explode('[mymovi-form-pagebreak]', $content);
+	
+	$err = false;
+	
+	if(isset($_POST['mymovi-submit']) && $_POST['mymovi-submit']) {
+		$output .= '
+		<div class="mymovi-message success">
+			<p>' . esc_html__('You have successfully submitted your data.', 'mymovi') . '</p>
+		</div>';
+		$output .= '<p>The following fields will be saved:</p><p>';
+		foreach($_POST as $key => $value) {
+			if(str_starts_with($key, 'mmv-field')) {
+				$output .= $key . ': ' . $value;
+			}
+		}
+		$output .= '</p>';
+		
+		$post = array(
+			'post_title'	=> esc_html__('Entry','mymovi') . ' ' . current_datetime()->format('Y-m-d H:i:s'),
+			'post_status'	=> 'draft',
+			'post_type'	=> 'mmv-survey-entry'
+		);
+		
+		$postid = wp_insert_post($post);
+		
+		foreach($_POST as $key => $value) {
+			if(str_starts_with($key, 'mmv-field')) {
+				add_post_meta($postid,$key,$value);
+			}
+		}
+	}
+	else {
+		$output .= '
+		<div class="wp-block-group has-global-padding">
+			<form class="mymovi-form" method="post" action="">
+				<div class="mymovi-form-pages-wrapper">
+					<div class="mymovi-form-page page-1">
+				';
+		$index = 1;
+		foreach($form_pages as $form_page) {
+			$output .= do_shortcode($form_page);
+			
+			// submit button for last page
+			if($index == sizeof($form_pages)) {
+				$output .= '
+						<div class="mymovi-form-submit wp-block-buttons">
+							<div class="wp-block-button">
+								<input class="wp-block-button__link wp-element-button" type="submit" name="mymovi-submit" value="'. esc_html__('Submit data', 'mymovi') . '">
+							</div>
+						</div>';
+			}
+				
+			$output .= '<div class="mymovi-nav-wrapper">
+							<div class="mymovi-nav prev wp-block-buttons">';
+			
+			// previous page button
+			if($index > 1) {
+				$output .= '
+								<div class="wp-block-button"><a class="wp-block-button__link wp-element-button mymovi-button" data-showpage="page-' . ($index - 1) . '" href="#">' . esc_html__('Prev', 'mymovi'). '</a></div>';
+			}
+			
+			$output .= '
+							</div>
+							<div class="mymovi-nav next wp-block-buttons">';
+						
+			// next page button
+			if($index < sizeof($form_pages)) {
+				$output .= '
+								<div class="wp-block-button"><a class="wp-block-button__link wp-element-button mymovi-button" data-showpage="page-' . ($index + 1) . '" href="#">' . esc_html__('Next', 'mymovi'). '</a></div>';
+			}
+			
+			// close nav wrapper
+			$output .= '
+							</div>
+						</div>';
+			
+			// current page indicator
+			$output .= '
+						<div class="mymovi-form-page-counter">
+							<p>' . sprintf(esc_html__('Page %1$s of %2$s','mymovi'), $index, sizeof($form_pages)) . '</p>
+						</div>';
+			
+			// close/open next page
+			if($index < sizeof($form_pages)) {
+				$output .= '
+					</div>
+					<div class="mymovi-form-page page-' . ($index + 1) . '">';
+			}
+			$index++;
+		}
+		$output .= '
+					</div>
+				</div>
+			 </form>
+		</div>';
+	}
+	
+	return $output;
+}
+add_shortcode('mymovi-form', 'mymovi_form_shortcode');
+
+function mymovi_form_field_shortcode($atts, $content) {
+	$output = '';
+	
+	/*$a = shortcode_atts( 
+		array(
+			'type' => 'text',
+			'name' => '',
+		), 
+		$atts
+	);*/
+	
+	$type = 'text';
+	$label = '';
+	
+	if(is_array($atts) && $atts) {
+		if(isset($atts[0]) && $atts[0]) $type = $atts[0];
+		if(isset($atts[1]) && $atts[1]) $name = $atts[1];
+		if(isset($atts[2]) && $atts[2]) $label = $atts[2];
+		
+	}	
+	
+	$label_output = array(
+		'<label>' . $label,
+		'</label>'
+	);
+	
+	switch($type) {
+		case 'text':
+			$output .= '
+			<div class="mymovi-form-field input-field type-text">
+				' . $label_output[0] . '<br>
+					<input type="text" name="mmv-field-' . $name . '">
+				' . $label_output[1] . '
+			</div>';
+			break;
+		case 'map':
+			$output .= '
+			<select class="form-select" id="select-geometry-type" name="geometry-type">
+				<option value="Point">Point</option>
+				<option value="LineString">LineString</option>
+				<option value="Polygon">Polygon</option>
+				<option value="Circle">Circle</option>
+				<option value="None">None</option>
+  			</select>
+ 			<input class="form-control" type="button" value="Remove last point (only for LineString and Polygon)" id="undo">
+			<div class="mymovi-form-field mymovi-map map-field type-map" id="' . $name . '"></div>
+			<button id="show-geojson">GeoJSON anzeigen</button>
+			<script> addMap("' . $name . '", true); </script>';
+
+			break;
+	}
+	return $output;
+}
+add_shortcode('mymovi-form-field','mymovi_form_field_shortcode');
+add_shortcode('mymovi-form-field*','mymovi_form_field_shortcode');
+
 
 /**
  * The core plugin class that is used to define internationalization,
@@ -79,3 +292,5 @@ function mymovi_run() {
 
 }
 mymovi_run();
+
+add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
--- public/js/mymovi-public.js
+++ public/js/mymovi-public.js
@@ -1,155 +1,187 @@
-const raster = new ol.layer.Tile({
-  source: new ol.source.OSM(),
-});
+function addMap(id, draw = false) {
+  if(document.getElementById(id)) {
+    const raster = new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    });
 
-const source = new ol.source.Vector({wrapX: false});
+    const source = new ol.source.Vector({wrapX: false});
 
-const vector = new ol.layer.Vector({
-  source: source,
-  style: {
-    'fill-color': 'rgba(230, 19, 126, .2)',
-    'stroke-color': 'rgb(230, 19, 126)',
-    'stroke-width': 2,
-    'circle-radius': 7,
-    'circle-fill-color': 'rgba(230, 19, 126, .8)',
-  },
-});
+    const vector = new ol.layer.Vector({
+      source: source,
+      style: {
+        'fill-color': 'rgba(230, 19, 126, .2)',
+        'stroke-color': 'rgb(230, 19, 126)',
+        'stroke-width': 2,
+        'circle-radius': 7,
+        'circle-fill-color': 'rgba(230, 19, 126, .8)',
+      },
+    });
 
-// Limit multi-world panning to one world east and west of the real world.
-// Geometry coordinates have to be within that range.
-/* TBD! Solve later
-const extent = ol.Object.get('EPSG:3857').getExtent().slice();
-extent[0] += extent[0];
-extent[2] += extent[2];
-*/
+    // Limit multi-world panning to one world east and west of the real world.
+    // Geometry coordinates have to be within that range.
+    /* TBD! Solve later
+    const extent = ol.Object.get('EPSG:3857').getExtent().slice();
+    extent[0] += extent[0];
+    extent[2] += extent[2];
+    */
 
-const map = new ol.Map({
-  target: 'ifl-map',
-  layers: [raster, vector],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([12.47209, 51.34680]),
-    zoom: 15,
-	// TBD! extent
-  }),
-});
+    const map = new ol.Map({
+      target: id,
+      layers: [raster, vector],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([12.47209, 51.34680]),
+        zoom: 15,
+      // TBD! extent
+      }),
+    });
 
-const modify = new ol.interaction.Modify({source: source});
-map.addInteraction(modify);
+    if(draw) {
+      const modify = new ol.interaction.Modify({source: source});
+      map.addInteraction(modify);
 
-let draw, snap; // global so we can remove them later
+      let draw, snap; // global so we can remove them later
 
-const typeSelect = document.getElementById('select-geometry-type');
+      const typeSelect = document.getElementById('select-geometry-type');
 
-function addInteractions() {
-  draw = new ol.interaction.Draw({
-    source: source,
-    type: typeSelect.value,
-  });
-  map.addInteraction(draw);
-  snap = new ol.interaction.Snap({
-	  source: source,
-  });
-  map.addInteraction(snap);
+      function addInteractions() {
+        draw = new ol.interaction.Draw({
+          source: source,
+          type: typeSelect.value,
+        });
+        map.addInteraction(draw);
+        snap = new ol.interaction.Snap({
+          source: source,
+        });
+        map.addInteraction(snap);
+      }
+
+      /**
+       * Handle change event.
+       */
+      typeSelect.onchange = function () {
+        map.removeInteraction(draw);
+        map.removeInteraction(snap);
+        addInteractions();
+      };
+
+      addInteractions();
+
+      document.getElementById('undo').addEventListener('click', function () {
+        draw.removeLastPoint();
+      });
+      
+      document.getElementById('show-geojson').addEventListener('click', function (e) {
+        e.preventDefault();
+        let geom = [];
+        source.forEachFeature( function(feature) { geom.push(new ol.Feature(feature.getGeometry().clone().transform('EPSG:3857', 'EPSG:4326'))); } );
+        let writer = new ol.format.GeoJSON();
+        let geoJsonStr = writer.writeFeatures(geom);
+        alert(geoJsonStr);
+      });
+
+      /*let points = [],
+          url_osrm_nearest = '//router.project-osrm.org/nearest/v1/driving/',
+          url_osrm_route = '//router.project-osrm.org/route/v1/driving/',
+          routeStyle = {
+            route: new ol.style.Style({
+              stroke: new ol.style.Stroke({
+                width: 6, color: [102, 102, 102, .7]
+              })
+            }),
+          };
+
+      map.on('click', function(evt){
+        if(typeSelect.value == 'Point') {
+          utils.getNearest(evt.coordinate).then(function(coord_street){
+          let last_point = points[points.length - 1];
+          let points_length = points.push(coord_street);
+
+            if(last_point) {
+              //get the route
+              let point1 = last_point.join();
+              let point2 = coord_street.join();
+
+              fetch(url_osrm_route + point1 + ';' + point2).then(function(r) { 
+                console.log(r);
+                return r.json();
+              }).then(function(json) {
+                if(json.code !== 'Ok') {
+                return;
+                }
+                //points.length = 0;
+                utils.createRoute(json.routes[0].geometry);
+
+                fullLength += json.routes[0].distance;
+
+                if(fullLength > 100) document.getElementById('street-distance').innerHTML = Math.round((fullLength / 1000) * 100) / 100 + ' ' + 'km';
+                else document.getElementById('street-distance').innerHTML = Math.round(fullLength * 100) / 100 + ' ' + 'm';
+              });
+            }
+          });
+        }
+      });
+
+      let fullLength = 0;
+
+      let utils = {
+        getNearest: function(coord){
+          var coord4326 = utils.to4326(coord);    
+          return new Promise(function(resolve, reject) {
+            //make sure the coord is on street
+            fetch(url_osrm_nearest + coord4326.join()).then(function(response) { 
+              // Convert to JSON
+              return response.json();
+            }).then(function(json) {
+              if (json.code === 'Ok') resolve(json.waypoints[0].location);
+              else reject();
+            });
+          });
+        },
+        createFeature: function(coord) {
+          var feature = new ol.Feature({
+            type: 'place',
+            geometry: new ol.geom.Point(ol.proj.fromLonLat(coord))
+          });
+          vectorSource.addFeature(feature);
+        },
+        createRoute: function(polyline) {
+          // route is ol.geom.LineString
+          var route = new ol.format.Polyline({
+            factor: 1e5
+          }).readGeometry(polyline, {
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+          });
+          var feature = new ol.Feature({
+            type: 'route',
+            geometry: route
+          });
+          feature.setStyle(routeStyle.route);
+          source.addFeature(feature);
+        },
+        to4326: function(coord) {
+          return ol.proj.transform([
+            parseFloat(coord[0]), parseFloat(coord[1])
+          ], 'EPSG:3857', 'EPSG:4326');
+        }
+      };*/
+    }
+  }
 }
 
-/**
- * Handle change event.
- */
-typeSelect.onchange = function () {
-  map.removeInteraction(draw);
-  map.removeInteraction(snap);
-  addInteractions();
-};
-
-addInteractions();
-
-document.getElementById('undo').addEventListener('click', function () {
-  draw.removeLastPoint();
-});
-
-let points = [],
-    url_osrm_nearest = '//router.project-osrm.org/nearest/v1/driving/',
-    url_osrm_route = '//router.project-osrm.org/route/v1/driving/',
-    routeStyle = {
-      route: new ol.style.Style({
-        stroke: new ol.style.Stroke({
-          width: 6, color: [102, 102, 102, .7]
-        })
-      }),
-    };
-
-map.on('click', function(evt){
-  if(typeSelect.value == 'Point') {
-	  utils.getNearest(evt.coordinate).then(function(coord_street){
-		let last_point = points[points.length - 1];
-		let points_length = points.push(coord_street);
-
-			if(last_point) {
-				//get the route
-				let point1 = last_point.join();
-				let point2 = coord_street.join();
-
-				fetch(url_osrm_route + point1 + ';' + point2).then(function(r) { 
-					console.log(r);
-				  return r.json();
-				}).then(function(json) {
-				  if(json.code !== 'Ok') {
-					return;
-				  }
-				  //points.length = 0;
-				  utils.createRoute(json.routes[0].geometry);
-					
-				  fullLength += json.routes[0].distance;
-	  
-				  if(fullLength > 100) document.getElementById('street-distance').innerHTML = Math.round((fullLength / 1000) * 100) / 100 + ' ' + 'km';
-				  else document.getElementById('street-distance').innerHTML = Math.round(fullLength * 100) / 100 + ' ' + 'm';
-				});
-			}
-		});
+document.addEventListener("DOMContentLoaded", () => {
+  /**
+   * MyMoVi form
+   */
+  if(document.querySelector('.mymovi-form')) {
+    document.querySelectorAll('.mymovi-form-page').forEach((item) => {item.style.display = 'none'});
+    document.querySelectorAll('.mymovi-form-page .mymovi-button').forEach((item) => {
+      item.onclick = (event) => {
+        event.preventDefault();
+        event.target.closest('.mymovi-form-page').style.display = "none";
+        document.querySelector('.mymovi-form-page.' + event.target.getAttribute('data-showpage')).style.display = 'block';
+      }
+    });
+    document.querySelector('.mymovi-form-page:first-child').style.display = 'block';
   }
 });
-
-let fullLength = 0;
-
-let utils = {
-  getNearest: function(coord){
-    var coord4326 = utils.to4326(coord);    
-    return new Promise(function(resolve, reject) {
-      //make sure the coord is on street
-      fetch(url_osrm_nearest + coord4326.join()).then(function(response) { 
-        // Convert to JSON
-        return response.json();
-      }).then(function(json) {
-        if (json.code === 'Ok') resolve(json.waypoints[0].location);
-        else reject();
-      });
-    });
-  },
-  createFeature: function(coord) {
-    var feature = new ol.Feature({
-      type: 'place',
-      geometry: new ol.geom.Point(ol.proj.fromLonLat(coord))
-    });
-    vectorSource.addFeature(feature);
-  },
-  createRoute: function(polyline) {
-    // route is ol.geom.LineString
-    var route = new ol.format.Polyline({
-      factor: 1e5
-    }).readGeometry(polyline, {
-      dataProjection: 'EPSG:4326',
-      featureProjection: 'EPSG:3857'
-    });
-    var feature = new ol.Feature({
-      type: 'route',
-      geometry: route
-    });
-    feature.setStyle(routeStyle.route);
-    source.addFeature(feature);
-  },
-  to4326: function(coord) {
-    return ol.proj.transform([
-      parseFloat(coord[0]), parseFloat(coord[1])
-    ], 'EPSG:3857', 'EPSG:4326');
-  }
-};
